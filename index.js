7eeda187639bfd12f02f4f6593d4f67217becab0
Zendovo <33803331+Zendovo@users.noreply.github.com>
2018-03-19 15:23:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,8 +22,66 @@ bot.on("message", async message => {
 
   if (cmd == `${prefix}setmsgchannel`) {
     var chid = args[0];
-    db.set('msgchannel', chid)
-    db.fetch('msgchannel').then ( id => message.channel.send(`Join/Leave Message channel set to ID: ${id}`));
+    db.set('msgchannel', chid);
+    db.fetch('msgchannel').then ( id =>
+      let msgchannelEmbed = new Discord.RichEmbed()
+      .setDescription(`Join/Leave Message channel set to ID: ${id}`)
+      message.channel.send(msgchannelEmbed));
+  }
+
+  if (cmd == `${prefix}msgchannel`) {
+    db.fetch('msgchannel').then ( id =>
+      let msgchannelEmbed = new Discord.RichEmbed()
+      .setDescription(`Join/Leave Message channel ID: ${id}`)
+      message.channel.send(msgchannelEmbed));
+  }
+
+  if (cmd == `${prefix}setjoinmessage`) {
+    var jmsg = args.join(" ");
+    db.set('joinmessage', jmsg);
+    db.fetch('joinmessage').then ( msg =>
+      let joinmsgEmbed = new Discord.RichEmbed()
+      .setDescription(`Join Message set to: ${msg}`)
+      message.channel.send(joinmsgEmbed));
+  }
+
+  if (cmd == `${prefix}joinmsg`) {
+    db.fetch('joinmessage').then ( msg =>
+      let joinmsgEmbed = new Discord.RichEmbed()
+      .setDescription(`Join Message: ${msg}`)
+      message.channel.send(joinmsgEmbed));
+  }
+
+  if (cmd == `${prefix}setleavemessage`) {
+    var lmsg = args.join(" ");
+    db.set('leavemessage', lmsg);
+    db.fetch('leavemessage').then ( msg =>
+      let leavemsgEmbed = new Discord.RichEmbed()
+      .setDescription(`Leave Message set to: ${msg}`)
+      message.channel.send(msgchannelEmbed));
+  }
+
+  if (cmd == `${prefix}leavemsg`) {
+    db.fetch('leavemessage').then ( msg =>
+      let leavemsgEmbed = new Discord.RichEmbed()
+      .setDescription(`Leave Message: ${msg}`)
+      message.channel.send(msgchannelEmbed));
+  }
+
+  if (cmd == `${prefix}setdmmessage`) {
+    var dmmsg = args.join(" ");
+    db.set('dmmessage', dmmsg);
+    db.fetch('dmmessage').then ( msg =>
+      let DMmsgEmbed = new Discord.RichEmbed()
+      .setDescription(`Join DM Message set to: ${msg}`)
+      message.channel.send(DMmsgEmbed));
+  }
+
+  if (cmd == `${prefix}dmmsg`) {
+    db.fetch('dmmessage').then ( msg =>
+      let DMmsgEmbed = new Discord.RichEmbed()
+      .setDescription(`Join DM Message: ${msg}`)
+      message.channel.send(DMmsgEmbed));
   }
 });
 
